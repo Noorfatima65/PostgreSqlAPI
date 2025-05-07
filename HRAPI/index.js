4d4cbb4f4ef61e86f5c9ce9317392821c29e41ed
Noorfatima65 <bscs2312165@szabist.pk>
2025-05-07 18:56:31 +0000

    Index.js and Script

--- HRAPI/index.js
+++ HRAPI/index.js
@@ -127,3 +127,175 @@ app.get('/totaljobs',async(req,res)=>{
         res.status(500).json({Error:err.message});
     }
 });
+
+app.get('/maxsal',async(req,res)=>{
+    try {
+        const result = await pool.query('select max(max_salary) from jobs');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/minsal',async(req,res)=>{
+    try {
+        const result = await pool.query('select min(min_salary) from jobs');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/max',async(req,res)=>{
+    try {
+        const result = await pool.query('select max(salary) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/min',async(req,res)=>{
+    try {
+        const result = await pool.query('select min(salary) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/avg',async(req,res)=>{
+    try {
+        const result = await pool.query('select avg(salary) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/maxcom',async(req,res)=>{
+    try {
+        const result = await pool.query('select max(commission_pct) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/mincom',async(req,res)=>{
+    try {
+        const result = await pool.query('select min(commission_pct) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/avgcom',async(req,res)=>{
+    try {
+        const result = await pool.query('select avg(commission_pct) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/totalmng',async(req,res)=>{
+    try {
+        const result = await pool.query('select count(manager_id) from departments');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/totalloc',async(req,res)=>{
+    try {
+        const result = await pool.query('select count(location_id) from departments');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+
+app.get('/totalreg',async(req,res)=>{
+    try {
+        const result = await pool.query('select count(region_id) from countries');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/totalmngg',async(req,res)=>{
+    try {
+        const result = await pool.query('select count(manager_id) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/totaldptt',async(req,res)=>{
+    try {
+        const result = await pool.query('select count(department_id) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/totalempp',async(req,res)=>{
+    try {
+        const result = await pool.query('select count(employee_id) from job_history');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/dpttotal',async(req,res)=>{
+    try {
+        const result = await pool.query('select count(department_id) from job_history');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/start',async(req,res)=>{
+    try {
+        const result = await pool.query('select max(start_date) from job_history');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/end',async(req,res)=>{
+    try {
+        const result = await pool.query('select min(end_date) from job_history');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/maxhire',async(req,res)=>{
+    try {
+        const result = await pool.query('select max(hire_date) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
+
+app.get('/minhire',async(req,res)=>{
+    try {
+        const result = await pool.query('select min(hire_date) from employees');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({Error:err.message});
+    }
+});
